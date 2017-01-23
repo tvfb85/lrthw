31826add842d06e591be303eca31f5bf3eb0b1b0
tvfb85 <vicki_uk@hotmail.com>
2017-01-23 08:01:13 +0000

    lrthw - ex7 (with comments)

--- chap01/ex7.rb
+++ chap01/ex7.rb
@@ -23,6 +23,6 @@ end12 = "r"
 
 
 # watch that print vs. puts on this line what's it do?
-# prints outputs the variables which are concatenated with the + operator, but it does not include a return at the end, so when the next line runs, the puts method outputs the next variables on the same line.
+# print outputs the variables which are concatenated with the + operator, but it does not include a return at the end, so when the next line runs, the puts method on line 27 outputs the next variables on the same line.
 print end1 + end2 + end3 + end4 + end5 + end6
 puts end7 + end8 + end9 + end10 + end11 + end12
