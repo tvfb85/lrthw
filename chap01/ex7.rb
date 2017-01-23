b160d5a17687696e179969e35a863ba3b84af0ba
tvfb85 <vicki_uk@hotmail.com>
2017-01-23 07:58:49 +0000

    lrthw - ex7

---
+++ chap01/ex7.rb
@@ -0,0 +1,28 @@
+# prints the following string
+puts "Mary had a little lamb."
+# prints a string with another string inserted
+puts "Its fleece was white as #{'snow'}."
+# prints another string
+puts "And everywhere that Mary went."
+# prints . ten times
+puts "." * 10 # what'd that do?
+
+# variable assignment to strings containing one letter
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+
+# watch that print vs. puts on this line what's it do?
+# prints outputs the variables which are concatenated with the + operator, but it does not include a return at the end, so when the next line runs, the puts method outputs the next variables on the same line.
+print end1 + end2 + end3 + end4 + end5 + end6
+puts end7 + end8 + end9 + end10 + end11 + end12
